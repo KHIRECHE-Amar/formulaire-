febad7b8c4e8307e9af9f0eeb49528bf3f03dfbb
KHIRECHE-Amar <122579561+KHIRECHE-Amar@users.noreply.github.com>
2023-02-16 23:50:39 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -62,7 +62,7 @@ if(isset($_POST['insc'])){
 			<br><br>
 			<input type="submit" name="insc" value="inscription" class="btn btn-success">
 
-			<br> le mot de passe doit contenir au moins 8 caractere une maj et miniscule et un caractere sepcial<br>
+			<br> le mot de passe doit contenir au moins 8 caractere une maj et minuscule et un caractere sepcial<br>
 		</div>
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 		
